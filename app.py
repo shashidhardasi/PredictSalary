d0e9d28151c54be8f1d9019f3705c841aeb39a54
shashidhardasi <71173636+shashidhardasi@users.noreply.github.com>
2020-09-13 13:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-finalmodel = pickle.load(open('SalaryPredict.pkl', 'rb'))
+model1 = pickle.load(open('SalaryPredict.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -15,7 +15,7 @@ def predict():
     For rendering results on HTML GUI
     '''
     exp = float(request.form['Experience'])
-    #prediction = finalmodel.predict(np.array(exp))
+    prediction = model1.predict(exp)
 
     
 
